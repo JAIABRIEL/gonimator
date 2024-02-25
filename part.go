b3a1525c95a789d487df8972f187d7edf0c9178b
Gabriel Jann <mail@jannga.org>
2024-02-25 14:55:00 +0100

    Add NewPlayer method to part

--- part.go
+++ part.go
@@ -8,3 +8,10 @@ type Part struct {
 	// Value is the number that is returned while this part is active
 	Value int
 }
+
+// NewPlayer returns a new PartPlayer based on a pointer to the part.
+func (p *Part) NewPlayer() *PartPlayer {
+	return &PartPlayer{
+		part: p,
+	}
+}
