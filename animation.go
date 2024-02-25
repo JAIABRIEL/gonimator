8b81dde9d9aab7a2f43842acc2d49696597b9741
Gabriel Jann <mail@jannga.org>
2024-02-25 15:03:01 +0100

    Add NewPlayer method to animation

--- animation.go
+++ animation.go
@@ -5,3 +5,16 @@ type Animation struct {
 	// Parts that will be played
 	Parts []*Part
 }
+
+// NewPlayer returns a new AnimationPlayer based on a pointer to the animation.
+func (a *Animation) NewPlayer() *AnimationPlayer {
+	ap := &AnimationPlayer{
+		parts: make([]*PartPlayer, len(a.Parts)),
+	}
+
+	for i := range ap.parts {
+		ap.parts[i] = a.Parts[i].NewPlayer()
+	}
+
+	return ap
+}
